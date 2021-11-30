b3336327f76f2f05c28304faf1e56bcb12649f13
brendongama <brendon_gama@hotmail.com>
2021-11-30 18:07:27 -0300

    Implementação do lombok

---
+++ src/main/java/com/brendon/models/Pessoa.java
@@ -0,0 +1,28 @@
+package com.brendon.models;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.springframework.hateoas.RepresentationModel;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Pessoa extends RepresentationModel<Pessoa> implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String nome;
+	private Integer idade;
+}
